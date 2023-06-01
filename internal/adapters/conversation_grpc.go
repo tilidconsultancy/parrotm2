e2f1221b6fc999f9f7373938417ae2cb0498ff9e
israel <israelsodano@gmail.com>
2023-06-01 17:55:06 -0300

    add tennant user

--- internal/adapters/conversation_grpc.go
+++ internal/adapters/conversation_grpc.go
@@ -132,12 +132,13 @@ func buildConversations(cvs ...domain.Conversation) []*gRPC.Conversation {
 	r := []*gRPC.Conversation{}
 	for _, c := range cvs {
 		r = append(r, &gRPC.Conversation{
-			Id:        c.Id.String(),
-			Tenant:    buildTenant(&c.Tenant),
-			User:      buildUser(&c.User),
-			Status:    string(c.Status),
-			CreatedAt: c.CreatedAt.String(),
-			UpdatedAt: c.UpdatedAt.String(),
+			Id:          c.Id.String(),
+			Tenant:      buildTenant(&c.Tenant),
+			TennantUser: buildTenantUser(c.TenantUser),
+			User:        buildUser(&c.User),
+			Status:      string(c.Status),
+			CreatedAt:   c.CreatedAt.String(),
+			UpdatedAt:   c.UpdatedAt.String(),
 		})
 	}
 	return r
